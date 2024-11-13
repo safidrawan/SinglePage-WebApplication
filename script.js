152dc936c331cf56c26bafa1078a3962562e2fff
Safidrawan <safidrawan@gmail.com>
2024-11-13 12:45:43 +0430

    Nav links effects

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function showSection(section) {
+function showSection(section , link) {
   const sections = document.querySelectorAll(".section");
   sections.forEach((section) => {
     section.classList.add("hidden");
     section.classList.remove("active");
   });
   document.getElementById(section).classList.add("active");
+
+  const links = document.querySelectorAll(".navLink");
+  links.forEach((link) => {
+    link.classList.remove("current");
+  });
+  document.getElementById(link).classList.add("current");
 }
 
 function sendMessage() {
@@ -18,7 +24,57 @@ function sendMessage() {
   setTimeout(() => {
     alert.style.display = "none";
     email.value = "";
-    name.value= "";
+    name.value = "";
     message.value = "";
   }, 3000);
+}
+
+class Product {
+  constructor(id, name, price, image) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  }
+}
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+//  Add func
+  addItem(product){
+    let existingItem = this.items.find(item => item.product.id === product.id);
+
+    if(existingItem){
+      existingItem.quantity++;
+    }else{
+      this.items.push({product,quantity:1});
+    }
+// Update cart 
+/*
+    updateCartView(){
+      const cartSection = document.getElementById('cart');
+      const itemContainer = document.createElement('dive');
+      itemContainer.classList.add('cartItems');
+
+      if(this.items.length===0){
+        cartSection.innerHTML += '<p>Your cart is empty.</p>';
+        cartSection.innerHTML += '<a href="#" class="btn" onclick="showSection(\'products\')">Back to Products page</a>';
+        return;
+      }
+
+      // this.items.forEach(item=>{
+
+      // });
+
+    }*/
+  }
+
+  
+
+
+
+
+
 }
