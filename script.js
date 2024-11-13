7fc3bfe46edc316bfb638658095d304a7802be4d
Safidrawan <safidrawan@gmail.com>
2024-11-13 11:29:20 +0430

    about and contact

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function showSection(section){
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(section => {
-        section.classList.add('hidden');
-        section.classList.remove('active');
-    });
-    document.getElementById(section).classList.add("active");
+function showSection(section) {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section) => {
+    section.classList.add("hidden");
+    section.classList.remove("active");
+  });
+  document.getElementById(section).classList.add("active");
+}
+
+function sendMessage() {
+  let email = document.getElementById("email");
+  let name = document.getElementById("name");
+  let message = document.getElementById("message");
+  let alert = document.getElementById("alert");
+
+  alert.innerHTML = `Thank you, we will get back to soon throught ${email.value}`;
+  alert.style.display = "block";
+  setTimeout(() => {
+    alert.style.display = "none";
+    email.value = "";
+    name.value= "";
+    message.value = "";
+  }, 3000);
 }
